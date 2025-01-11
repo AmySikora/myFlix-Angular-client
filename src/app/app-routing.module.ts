74107cc3cc7848d7df9a7d37cc58107fffb32812
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-01-10 21:58:09 -0800

    updated app-routing module to add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { GenreComponent } from './genre/genre.component';
+import { DirectorComponent } from './director/director.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
 
 const routes: Routes = [
-   {path: '', component: WelcomePageComponent },
+   {path: 'welcome', component: WelcomePageComponent },
+   {path: 'movies', component: MovieCardComponent},
+   {path: 'genre', component: GenreComponent},
+   {path: 'director', component: DirectorComponent},
+   {path: 'synopsis', component: SynopsisComponent},
 ];
 
 @NgModule({

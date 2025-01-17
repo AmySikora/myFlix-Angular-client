dfd0521fbf75aa1e579c89c7a47f4f804a192cf5
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-01-16 22:22:19 -0800

    added comments

--- src/app/info-dialog/info-dialog.component.ts
+++ src/app/info-dialog/info-dialog.component.ts
@@ -1,22 +1,38 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+/**
+ * A reusable dialog component for displaying information such as genre, director, or movie description.
+ * This component is used to display a pop-up modal with a title and content passed via `MAT_DIALOG_DATA`.
+ */
 @Component({
   selector: 'app-info-dialog',
   templateUrl: './info-dialog.component.html',
   styleUrls: ['./info-dialog.component.scss'],
 })
 export class InfoDialogComponent implements OnInit {
+  /**
+   * Creates an instance of InfoDialogComponent.
+   * @param data - The data injected into the dialog, including the title and content to display.
+   * @param dialogRef - Reference to the currently open dialog, used to close it.
+   */
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: { title: string; content: string },
     public dialogRef: MatDialogRef<InfoDialogComponent>
   ) {}
 
+  /**
+   * Lifecycle hook that is called after data-bound properties are initialized.
+   * Logs the dialog data to the console.
+   */
   ngOnInit(): void {
-    console.log('Dialog Data:', this.data); 
+    console.log('Dialog Data:', this.data);
   }
 
+  /**
+   * Closes the dialog.
+   */
   closeDialog(): void {
     this.dialogRef.close();
   }

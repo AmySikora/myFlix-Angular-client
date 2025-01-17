1d9693ec1a45bb17aa2b725ca85da4cf0d4a71d1
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-01-17 13:19:41 -0800

    added comments to welcome page

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,3 +1,8 @@
+/**
+ * Component responsible for displaying the welcome page.
+ * Provides dialogs for user registration and login.
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
@@ -6,19 +11,34 @@ import { UserLoginFormComponent } from '../user-login-form/user-login-form.compo
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent implements OnInit {
+  /**
+   * Creates an instance of WelcomePageComponent.
+   * @param dialog - Service to open Angular Material dialogs.
+   */
   constructor(public dialog: MatDialog) {}
 
+  /**
+   * Lifecycle hook that is called after the component is initialized.
+   */
   ngOnInit(): void {}
 
+  /**
+   * Opens the user registration dialog.
+   * Displays the `UserRegistrationFormComponent` in a modal dialog.
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px',
     });
   }
 
+  /**
+   * Opens the user login dialog.
+   * Displays the `UserLoginFormComponent` in a modal dialog.
+   */
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px',
